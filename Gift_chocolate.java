d43c1a33df3f97a5cd47b510679c024e18d0d7e3
Madhurima Saha <mourima301098@gmail.com>
2020-02-23 11:03:52 +0530

    Update Gift_chocolate.java

--- Gift_chocolate.java
+++ Gift_chocolate.java
@@ -2,7 +2,7 @@ package gifts_chocolate;
 class Gift__chocolate
 {
 public int price,weight;
-public Gift_chocolate(String price,String weight)
+public Gift_chocolate(int price,int weight)
 {
 this.price=price;
 this.weight=weight;
